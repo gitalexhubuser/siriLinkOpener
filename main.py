1a1777376ffeca569653dab648caf96141a8108f
gitalexhubuser <alexdubovyck@yandex.ru>
2023-10-27 07:40:33 +0300

    release (но скорость медленная)

--- main.py
+++ main.py
@@ -25,36 +25,30 @@ def AdobePremierePro_Helper():
     # pyperclip.paste()
 
 # работает !
-CHUNK = 4096 # 4096 8192
+CHUNK = 4096 ## 4096 8192
 mic = pyaudio.PyAudio()
-# stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True,  frames_per_buffer=8192)
+# stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
 stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=CHUNK)
 playsound("succes.mp3")
 
+
+def process_result(result):
+    if result in ["тест", "тэст"]:
+        playsound("succes.mp3")
+        webbrowser.open_new("https://www.youtube.com/@LuaNaZakaz")
+    elif result in ["дота", "пота"]:
+        playsound("succes.mp3")
+        subprocess.Popen(r"D:\Steam\steamapps\common\dota 2 beta\game\bin\win64\dota2.exe")
+    elif result in ["проект", "проекте", "проэкт"]:
+        playsound("succes.mp3")
+
+# Основной цикл
 while True:
-    try:
+    if keyboard.is_pressed('ctrl'):
         data = stream.read(4096) # 4096
+
         if recognizer.AcceptWaveform(data):
             text = recognizer.Result()
-
-            if keyboard.is_pressed('ctrl'):
-                result = text[14:-3]
-                print("result:", result)
-
-                if result == "тест" or result == "тэст":
-                    playsound("succes.mp3")
-                    webbrowser.open_new("https://www.youtube.com/@LuaNaZakaz")
-
-                if result == "дота" or result == "пота":
-                    playsound("succes.mp3")
-                    subprocess.Popen(r"D:\Steam\steamapps\common\dota 2 beta\game\bin\win64\dota2.exe")
-
-                if result == "проект" or result == "проекте" or result == "проэкт":
-                    playsound("succes.mp3")
-                    AdobePremierePro_Helper()
-                    pyperclip.paste()
-
-    except:
-        pass
-
-# stream.close()
+            result = text[14:-3]
+            print("result:", result)
+            process_result(result)
